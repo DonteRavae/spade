670b1c4071b55018f42f6ef23dbadbfbbde1f151
Donte Littlejohn <donteravae@gmail.com>
2024-01-09 13:27:26 -0500

    root html component built with styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
+// REMIX
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,13 +8,29 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  json,
+  useLoaderData,
 } from "@remix-run/react";
+// INTERNAL
+import NavBar from "./components/NavBar/NavBar";
+import Searchbar from "./components/Searchbar/Searchbar";
+import AccountDropdown from "./components/AccountDropdown/AccountDropdown";
+// STYLES
+import styles from "./root.module.css";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  return json({ query: q });
+};
+
 export default function App() {
+  const { query } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -22,7 +39,12 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body id={styles["app-body"]}>
+        <header id={styles["app-header"]}>
+          <Searchbar value={query} />
+          <NavBar />
+          <AccountDropdown />
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
