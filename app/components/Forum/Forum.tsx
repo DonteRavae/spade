b4fbb926d78c6b783b7bf5611e390b3edf73b19c
Donte Littlejohn <donteravae@gmail.com>
2024-01-11 22:49:39 -0500

    added dynamic data integration

--- app/components/Forum/Forum.tsx
+++ app/components/Forum/Forum.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable jsx-a11y/media-has-caption */
+import { useEffect } from "react";
+import forumData from "../../../utils/data/forum.json";
 // STYLES
 import styles from "./Forum.module.css";
 
@@ -13,25 +15,45 @@ import styles from "./Forum.module.css";
  *   -- Title
  *   -- Content
  *   -- Posted Timestamp
- * @returns
  */
 
-const ForumItemCard = () => {
+type ForumItem = {
+  title: string;
+  content: {
+    type: string;
+    value: string;
+  };
+  meta: {
+    submittedBy: {
+      alias: string;
+      avatar: string;
+    };
+    votes: number;
+    comments: number;
+    posted: string;
+    favorites: number;
+  };
+};
+
+const ForumItemCard = ({ item, flair }: { item: ForumItem; flair: string }) => {
+  const { title, content } = item;
+  const { submittedBy, votes, comments, posted } = item.meta;
+  const { alias, avatar } = submittedBy;
   return (
     <li className={styles["forum-item-card"]}>
       <div className={styles.sidebar}>
         <button className={styles["submitted-by"]}>
-          <img src="" alt="Name's Avatar" />
-          <p>Name</p>
+          <img src={avatar} alt="Name's Avatar" />
+          <p>{alias}</p>
         </button>
-        <button className={styles.sidebarItem}>Flair</button>
-        <div className={styles.sidebarItem}>Votes</div>
-        <p className={styles.sidebarItem}>Comments</p>
+        <button className={styles.sidebarItem}>{flair.toUpperCase()}</button>
+        <div className={styles.sidebarItem}>{votes}</div>
+        <p className={styles.sidebarItem}>{comments}</p>
         <div className={styles.sidebarItem}>Share</div>
         <button className={styles.sidebarItem}>Favorite</button>
       </div>
       <div className={styles["content-container"]}>
-        <h1 className={styles.title}>Title goes here</h1>
+        <h1 className={styles.title}>{title}</h1>
         {/* 
         -- CONTENT --
            
@@ -40,27 +62,30 @@ const ForumItemCard = () => {
 
         {/* <img className={styles.content} src="" alt="Post Content"/> */}
         {/* <video className={styles.content} src=""/> */}
-        <p className={styles.content}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-          quaerat quas dolore necessitatibus, minus tempora. Tempora quisquam
-          voluptates iure dolor sunt accusamus, fugit expedita at tempore nihil
-          quas ducimus laboriosam?
-        </p>
-        <p className={styles.posted}>Posted 6 hours ago</p>
+        {content.type === "text" ? (
+          <p className={styles.content}>{content.value}</p>
+        ) : null}
+        <p className={styles.posted}>Posted {posted}</p>
       </div>
     </li>
   );
 };
 
 export default function Forum() {
+  const { anxiety } = forumData;
+  useEffect(() => {
+    console.log(anxiety);
+  }, [anxiety]);
+
   return (
     <ul id={styles["forum"]}>
-      <ForumItemCard />
-      <ForumItemCard />
-      <ForumItemCard />
-      <ForumItemCard />
-      <ForumItemCard />
-      <ForumItemCard />
+      {anxiety.map((item) => (
+        <ForumItemCard
+          key={item.meta.submittedBy.alias}
+          item={item}
+          flair="ANXIETY"
+        />
+      ))}
     </ul>
   );
 }
