dc2b20246e6347ebff892350222383a073c3762e
Donte Littlejohn <donteravae@gmail.com>
2024-01-15 16:01:27 -0500

    wrapped forum item component footer buttons in links and added link styling

--- app/components/Forum/Forum.tsx
+++ app/components/Forum/Forum.tsx
@@ -1,5 +1,9 @@
 /* eslint-disable jsx-a11y/media-has-caption */
+
+// REACT
 import { useEffect } from "react";
+// REMIX
+import { Link, useFetcher } from "@remix-run/react";
 // INTERNAL
 import Icons from "../Icons";
 import forumData from "../../utils/db/forum.json";
@@ -20,6 +24,7 @@ import styles from "./Forum.module.css";
  */
 
 type ForumItem = {
+  id: string;
   title: string;
   content: {
     type: string;
@@ -33,63 +38,98 @@ type ForumItem = {
     votes: number;
     comments: number;
     posted: string;
-    favorites: number;
+    favorite: boolean;
+    flair: string;
   };
 };
 
-const ForumItemCard = ({ item, flair }: { item: ForumItem; flair: string }) => {
-  const { title, content } = item;
-  const { submittedBy, votes, comments, posted } = item.meta;
+const ForumItemCard = ({ item }: { item: ForumItem }) => {
+  const { id, title, content } = item;
+  const { submittedBy, votes, comments, posted, flair, favorite } = item.meta;
   const { alias, avatar } = submittedBy;
+  const fetcher = useFetcher();
   return (
-    <li className={styles["forum-item-card"]}>
-      <button className={styles["sidebar"]}>
-        <img src={avatar} alt="Name's Avatar" />
-        <p className={styles.alias}>{alias}</p>
-        <p className={styles.posted}>{posted}</p>
-      </button>
+    <li>
+      <Link
+        className={styles["forum-item-card"]}
+        to={`/community/${flair}/${id}`}
+      >
+        <Link className={styles["sidebar"]} to={`/community/u/${alias}`}>
+          <img src={avatar} alt="Name's Avatar" />
+          <p className={styles.alias}>{alias}</p>
+          <p className={styles.posted}>{posted}</p>
+        </Link>
 
-      <div className={styles["content-container"]}>
-        <h1 className={styles.title}>{title}</h1>
+        <div className={styles["content-container"]}>
+          <h1 className={styles.title}>{title}</h1>
 
-        {content.type === "text" && content.value ? (
-          <p className={styles.content}>{content.value}</p>
-        ) : content.type === "image" && content.value ? (
-          <img className={styles.content} src="" alt="Post Content" />
-        ) : content.type === "video" && content.value ? (
-          <video className={styles.content} src="" />
-        ) : null}
-        <footer>
-          <div className={styles["footer-item"]}>
-            <button className={styles.upvote}>
-              <Icons type="caret-up" />
+          {content.type === "text" && content.value ? (
+            <p className={styles.content}>{content.value}</p>
+          ) : content.type === "image" && content.value ? (
+            <img className={styles.content} src="" alt="Post Content" />
+          ) : content.type === "video" && content.value ? (
+            <video className={styles.content} src="" />
+          ) : null}
+          <footer>
+            <fetcher.Form className={styles["footer-item"]} method="post">
+              <input hidden readOnly value="vote-update" name="request-type" />
+              <button
+                className={styles.upvote}
+                aria-label="Upvote post"
+                onClick={(event) => event.preventDefault()}
+              >
+                <Icons type="caret-up" />
+              </button>
+              {votes}
+              <button
+                className={styles.downvote}
+                aria-label="Downvote post"
+                onClick={(event) => event.preventDefault()}
+              >
+                <Icons type="caret-down" />
+              </button>
+            </fetcher.Form>
+            <Link
+              to={`/community/${flair}/${id}/#comments`}
+              className={`${styles["footer-item"]} ${styles.comments}`}
+            >
+              <Icons type="comment" />
+              {comments}
+            </Link>
+            <button className={`${styles["footer-item"]} ${styles.share}`}>
+              <Icons type="share" />
+              Share
             </button>
-            {votes}
-            <button className={styles.downvote}>
-              <Icons type="caret-down" />
-            </button>
-          </div>
-          <button className={`${styles["footer-item"]} ${styles.comments}`}>
-            <Icons type="comment" />
-            {comments}
-          </button>
-          <button className={`${styles["footer-item"]} ${styles.share}`}>
-            <Icons type="share" />
-            Share
-          </button>
-          <button className={`${styles["footer-item"]} ${styles.favorite}`}>
-            <Icons type="empty-heart" />
-            Favorite
-          </button>
-          <button
-            className={`${styles["footer-item"]} ${
-              styles[flair.toLowerCase()]
-            } ${styles.flair}`}
-          >
-            {flair.toUpperCase()}
-          </button>
-        </footer>
-      </div>
+            <fetcher.Form
+              method="post"
+              className={`${styles["footer-item"]} ${styles.favorite}`}
+            >
+              <input hidden readOnly value="vote-update" name="request-type" />
+              <button
+                onClick={(event) => event.preventDefault()}
+                aria-label={
+                  favorite ? "Remove from favorites" : "Add to favorites"
+                }
+              >
+                {!favorite ? (
+                  <Icons type="empty-heart" />
+                ) : (
+                  <Icons type="full-heart" />
+                )}
+                Favorite
+              </button>
+            </fetcher.Form>
+            <Link
+              to={`/community/${flair}`}
+              className={`${styles["footer-item"]} ${
+                styles[flair.toLowerCase()]
+              } ${styles.flair}`}
+            >
+              {flair}
+            </Link>
+          </footer>
+        </div>
+      </Link>
     </li>
   );
 };
@@ -103,11 +143,7 @@ export default function Forum() {
   return (
     <ul id={styles["forum"]}>
       {anxiety.map((item) => (
-        <ForumItemCard
-          key={item.meta.submittedBy.alias}
-          item={item}
-          flair="ANXIETY"
-        />
+        <ForumItemCard key={item.meta.submittedBy.alias} item={item} />
       ))}
     </ul>
   );
