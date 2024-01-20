151f2120ab913ec8e0f85991bea6af0cca58d7c5
Donte Littlejohn <donteravae@gmail.com>
2024-01-20 17:42:09 -0500

    added styling and a logout button

--- app/components/AccountDropdown/AccountDropdown.tsx
+++ app/components/AccountDropdown/AccountDropdown.tsx
@@ -1,5 +1,10 @@
 // STYLES
+import { useState } from "react";
+import Icons from "../Icons";
 import styles from "./AccountDropdown.module.css";
+import { Form, useFetcher } from "@remix-run/react";
+import auth from "~/utils/db/auth/config";
+import { signOut } from "firebase/auth";
 
 export default function AccountDropdown({
   username,
@@ -8,13 +13,42 @@ export default function AccountDropdown({
   username: string;
   avatar: string;
 }) {
+  const fetcher = useFetcher();
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+  const handleClick = () => setShowMenu((prev) => !prev);
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    fetcher.submit({ "logout-user": true }, { method: "post" });
+  };
+
   return (
-    <>
-      <button id={styles["account-dropdown-btn"]}>
+    <div id={styles["account-dropdown-container"]}>
+      <button
+        id={styles["account-dropdown-btn"]}
+        className={showMenu ? styles.show : ""}
+        onClick={handleClick}
+      >
         <h4>{username}</h4>
         <img src={avatar} alt="Name's Avatar" />
+        <Icons type="caret-down" />
       </button>
-      <menu id={styles["account-dropdown-menu"]}></menu>
-    </>
+      <menu
+        id={styles["account-dropdown-menu"]}
+        className={showMenu ? styles.show : ""}
+      >
+        <Form method="post">
+          <button
+            id={styles["logout-btn"]}
+            type="submit"
+            onClick={handleLogout}
+          >
+            <Icons type="logout" />
+            Logout
+          </button>
+        </Form>
+      </menu>
+    </div>
   );
 }
