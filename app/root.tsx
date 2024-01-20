d5adb9608175eae2e24d5ef66fd43b3c579effed
Donte Littlejohn <donteravae@gmail.com>
2024-01-19 21:13:40 -0500

    added profile retrieval to application header; added styling to login/register links

--- app/root.tsx
+++ app/root.tsx
@@ -2,6 +2,7 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -17,19 +18,26 @@ import Searchbar from "./components/Searchbar/Searchbar";
 import AccountDropdown from "./components/AccountDropdown/AccountDropdown";
 // STYLES
 import styles from "./root.module.css";
+import { Profile, retrieveProfile } from "./utils/db/auth/auth.server";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
+  // PROFILE
+  const profile = await retrieveProfile(request);
+
+  // SEARCH
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
-  return json({ query: q });
+  return json({ profile, query: q });
 };
 
 export default function App() {
   const { query } = useLoaderData<typeof loader>();
+  let { profile } = useLoaderData<typeof loader>();
+  profile = profile ? (profile as Profile) : null;
 
   return (
     <html lang="en">
@@ -43,7 +51,18 @@ export default function App() {
         <header id={styles["app-header"]}>
           <Searchbar value={query} />
           <NavBar />
-          <AccountDropdown />
+          {!profile ? (
+            <div className={styles.accessLinks}>
+              <Link to="/login">Login</Link>
+              <hr />
+              <Link to="/register">Register</Link>
+            </div>
+          ) : (
+            <AccountDropdown
+              avatar={profile!.avatar}
+              username={profile?.username}
+            />
+          )}
         </header>
         <Outlet />
         <ScrollRestoration />
