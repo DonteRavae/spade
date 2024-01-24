45b0e11a181d101ae288b2ae5597cde27111a3be
Donte Littlejohn <donteravae@gmail.com>
2024-01-24 13:01:27 -0500

    moved meta details for home page

--- app/routes/_home.tsx
+++ app/routes/_home.tsx
@@ -1,14 +1,29 @@
-import CommunityOverview from "~/components/CommunityOverview/CommunityOverview";
+// REMIX
+import { MetaFunction } from "@remix-run/node";
+// INTERNAL
 import PageContainer from "~/components/PageContainer/PageContainer";
 import PodcastOverview from "~/components/Podcast Overview/Podcast Overview";
+import CommunityOverview from "~/components/CommunityOverview/CommunityOverview";
+// STYLES
 import styles from "./styles/Home.module.css";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "SPADE Mental Health | Home" },
+    {
+      name: "description",
+      content:
+        "SPADE Mental Health provides Suicide, PTSD, Anxiety, Depression, and Epilepsy awareness for the African American community.",
+    },
+  ];
+};
 
 export default function HomeLayout() {
   return (
     <PageContainer id={styles["home-page"]}>
       <PodcastOverview />
       <CommunityOverview />
-      <section>EXTRAS</section>
+      <aside>EXTRAS</aside>
     </PageContainer>
   );
 }
