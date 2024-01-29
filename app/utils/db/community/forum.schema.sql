6f3ab32466770aa20338b55610d07121937cfb27
Donte Littlejohn <donteravae@gmail.com>
2024-01-29 17:27:13 -0500

    added profiles and comments tables to the community db schema

--- app/utils/db/community/forum.schema.sql
+++ app/utils/db/community/forum.schema.sql
@@ -1,14 +1,40 @@
-CREATE TABLE posts (
+CREATE TABLE profiles (
+    id VARCHAR(100) UNIQUE NOT NULL,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    avatar_url VARCHAR(350) NOT NULL
+);
+
+CREATE TABLE forum_posts (
     id VARCHAR(100) PRIMARY KEY NOT NULL,
-    title TEXT NOT NULL,
+    title VARCHAR(300) NOT NULL,
     content_type VARCHAR(10),
     content TEXT,
     flair VARCHAR(50) NOT NULL,
     votes INT DEFAULT 1,
     favorite BOOLEAN DEFAULT FALSE,
     favorite_count INT DEFAULT 0,
-    posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    submitted_by VARCHAR(100),
-    FOREIGN KEY (submitted_by) REFERENCES profiles(id) ON UPDATE CASCADE ON DELETE SET NULL
+    submitted_by VARCHAR(100)
 );
+
+CREATE TABLE comments(
+    id VARCHAR(100) PRIMARY KEY NOT NULL,
+    content TEXT,
+    votes INT DEFAULT 1,
+    parent_comment VARCHAR(100),
+    parent_post VARCHAR(100) NOT NULL,
+    submitted_by VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (parent_post) REFERENCES forum_posts(id) ON DELETE CASCADE
+);
+
+-- Retrieve all comments Query
+-- WITH RECURSIVE ForumPostComments AS (
+--     -- Anchor Member Definition
+--     SELECT 
+--     -- Recursive Member Definition
+-- ) 
+-- -- Statement to execute CTE
+-- SELECT * FROM ForumPostComments;
