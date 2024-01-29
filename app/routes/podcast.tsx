4fcd23e33a27315308d3ef06fe1cd86b2b4a9ab3
Donte Littlejohn <donteravae@gmail.com>
2024-01-29 17:36:42 -0500

    created placeholder pages for podcast details and individual community post

---
+++ app/routes/podcast.tsx
@@ -0,0 +1,10 @@
+// INTERNAL
+import PageContainer from "~/components/PageContainer/PageContainer";
+// STYLES
+import styles from "./styles/Podcast.module.css";
+
+export default function podcast() {
+  return (
+    <PageContainer id={styles["podcast-page"]}>PODCAST PAGE</PageContainer>
+  );
+}
