59b5fa1a5526eb5dde156cf70c2b2c96d315659b
Donte Littlejohn <donteravae@gmail.com>
2024-01-30 13:09:01 -0500

    removed firestore export

--- app/utils/db/auth/config.ts
+++ app/utils/db/auth/config.ts
@@ -3,15 +3,10 @@ import { getAuth } from "firebase/auth";
 
 // Firebase configuration
 import firebaseConfig from "./firebaseConfig.json";
-import { Firestore, getFirestore } from "firebase/firestore";
 
 /**
  * Initialize Firebase Admin
  */
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
-const db: Firestore = getFirestore(app);
-
-export { db };
-
 export default getAuth(app);
