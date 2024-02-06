b06f501e0948fc10fc9126e761dea9e7d3b788a0
Donte Littlejohn <donteravae@gmail.com>
2024-02-05 21:58:29 -0500

    refactored comments and forum_posts schemas; created schemas for votes and favorites

--- app/utils/db/community/forum.schema.sql
+++ app/utils/db/community/forum.schema.sql
@@ -9,10 +9,7 @@ CREATE TABLE forum_posts (
     title VARCHAR(300) NOT NULL,
     content_type VARCHAR(10),
     content TEXT,
-    flair VARCHAR(50) NOT NULL,
-    votes INT DEFAULT 1,
-    favorite BOOLEAN DEFAULT FALSE,
-    favorite_count INT DEFAULT 0,
+    category VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     submitted_by VARCHAR(100)
@@ -20,14 +17,30 @@ CREATE TABLE forum_posts (
 
 CREATE TABLE comments(
     id VARCHAR(100) PRIMARY KEY NOT NULL,
-    content TEXT,
-    votes INT DEFAULT 1,
-    parent_comment VARCHAR(100),
-    parent_post VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    content_type VARCHAR(10),
+    parent_comment_id VARCHAR(100),
+    parent_post_id VARCHAR(100) NOT NULL,
     submitted_by VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (parent_post) REFERENCES forum_posts(id) ON DELETE CASCADE
+    FOREIGN KEY (parent_post_id) REFERENCES forum_posts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE votes (
+    id VARCHAR(100) PRIMARY KEY NOT NULL,
+    parent_id VARCHAR(100) NOT NULL,
+    vote TINYINT DEFAULT 0,
+    voter VARCHAR(100) NOT NULL,
+    FOREIGN KEY (voter) REFERENCES profiles(id) ON DELETE CASCADE
+);
+
+CREATE TABLE favorites (
+    parent_id VARCHAR(100) NOT NULL,
+    user_id VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES profiles(id) ON DELETE CASCADE
 );
 
 -- Retrieve all comments Query
