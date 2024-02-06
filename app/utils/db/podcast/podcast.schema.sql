7d9c059142e6147acd6ab3f37764cabddb64e3ce
Donte Littlejohn <donteravae@gmail.com>
2024-02-05 22:03:00 -0500

    created schemas for podcasts queries

---
+++ app/utils/db/podcast/podcast.schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE guest_requests (
+    name VARCHAR(200) NOT NULL,
+    occupation VARCHAR(200),
+    contact_info VARCHAR(200) NOT NULL,
+    request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE topic_requests (
+    topic TEXT NOT NULL,
+    request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
