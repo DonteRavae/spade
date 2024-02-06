7db1385e9629810965a6fb0fb63f669e6a495704
Donte Littlejohn <donteravae@gmail.com>
2024-02-05 22:08:53 -0500

    refactored action function

--- app/routes/_home._index.tsx
+++ app/routes/_home._index.tsx
@@ -1,8 +1,14 @@
 // REMIX
+import { ActionFunctionArgs } from "@remix-run/node";
 import { json, useLoaderData } from "@remix-run/react";
 // INTERNAL
 import Forum from "~/components/Forum/Forum";
+import { parseRequests } from "~/utils/helpers";
 import * as handlers from "~/utils/db/community/handlers.server";
+
+export const action = async ({request}: ActionFunctionArgs) => {
+  return await parseRequests(request);
+}
 
 export const loader = async () => {
   // Retrieve and return 20 most recent posts
