089736b4f67cccedf398c3cec61f36b2aecf0d1a
Donte Littlejohn <donteravae@gmail.com>
2024-02-05 22:00:32 -0500

    moved mysql config to separate file

---
+++ app/utils/db/mysql.config.ts
@@ -0,0 +1,13 @@
+// EXTERNAL
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 20,
+  waitForConnections: true,
+});
+
+export default pool;
