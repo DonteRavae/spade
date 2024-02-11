d6c66d3a7d8b0ae479d7c570261a59555716d7fc
Donte Littlejohn <donteravae@gmail.com>
2024-02-10 23:25:02 -0500

    added parse request route handler to recent page action

--- app/routes/_home.recent.tsx
+++ app/routes/_home.recent.tsx
@@ -1,9 +1,14 @@
 // REMIX
-import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { ActionFunctionArgs, json } from "@remix-run/node";
 // INTERNAL
 import Forum from "~/components/Forum/Forum";
+import { parseRequests } from "~/utils/db/helpers";
 import * as handlers from "~/utils/db/community/handlers.server";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  return await parseRequests(request);
+};
 
 export const loader = async () => {
   // Retrieve and return 20 most recent posts
