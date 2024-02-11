df1ec42a8c7c3618aa8f32e7705e5910d5f7063d
Donte Littlejohn <donteravae@gmail.com>
2024-02-11 00:28:17 -0500

    added route validation for guest request and topic request

--- app/utils/db/podcast/handlers.server.ts
+++ app/utils/db/podcast/handlers.server.ts
@@ -1,15 +1,41 @@
 import { requestGuest, requestTopic } from "./db.server";
+import { EMAIL_VALIDATION, URL_VALIDATION } from "../helpers";
 
 export const createGuestRequest = async (formData: FormData) => {
   const guestName = formData.get("guestName") as string;
   const guestOccupation = formData.get("guestOccupation") as string;
   const guestContact = formData.get("guestContact") as string;
 
+  if (!guestName)
+    return {
+      action: "guest-request",
+      success: false,
+      message: "Please enter a valid guest name.",
+    };
+
+  if (
+    !guestContact &&
+    !EMAIL_VALIDATION.test(guestContact) &&
+    !URL_VALIDATION.test(guestContact)
+  )
+    return {
+      action: "guest-request",
+      success: false,
+      message: "Please enter a valid guest contact.",
+    };
+
   return await requestGuest(guestName, guestOccupation, guestContact);
 };
 
 export const createTopicRequest = async (formData: FormData) => {
   const topic = formData.get("topic") as string;
+
+  if (!topic)
+    return {
+      action: "topic-request",
+      success: false,
+      message: "Please enter a valid topic request.",
+    };
 
   return await requestTopic(topic);
 };
