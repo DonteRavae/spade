f0cf296dd3fffe45b51069f5fb51ee7f7e442569
Donte Littlejohn <donteravae@gmail.com>
2024-02-10 23:24:13 -0500

    added optional classname prop to UserAvatar component

--- app/components/UserAvatar/UserAvatar.tsx
+++ app/components/UserAvatar/UserAvatar.tsx
@@ -5,12 +5,19 @@ export default function UserAvatar({
   id,
   avatarUrl,
   avatarAlt,
+  className,
 }: {
   id?: string;
   avatarUrl: string;
   avatarAlt: string;
+  className?: string;
 }) {
   return (
-    <img id={id} src={avatarUrl} alt={avatarAlt} className={styles.avatar} />
+    <img
+      id={id}
+      src={avatarUrl}
+      alt={avatarAlt}
+      className={`${styles.avatar} ${className}`}
+    />
   );
 }
