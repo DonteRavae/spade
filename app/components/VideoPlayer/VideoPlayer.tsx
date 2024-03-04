e6185aae12f9488c8eca0ad9302c6e9433aa3ae3
Donte Littlejohn <donteravae@gmail.com>
2024-03-04 13:55:43 -0500

    ran prettier

--- app/components/VideoPlayer/VideoPlayer.tsx
+++ app/components/VideoPlayer/VideoPlayer.tsx
@@ -1,15 +1,21 @@
 // EXTERNAL
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 // STYLES
 import styles from "./VideoPlayer.module.css";
 
 // @ts-expect-error bundling issue with react-player
 const Player = ReactPlayer.default as typeof ReactPlayer;
 
-export default function VideoPlayer({url}: {url: string}) {
+export default function VideoPlayer({ url }: { url: string }) {
   return (
     <div className={styles["video-player"]}>
-        <Player url={url} className={styles["react-player"]} width="100%" height="100%" controls />
+      <Player
+        url={url}
+        className={styles["react-player"]}
+        width="100%"
+        height="100%"
+        controls
+      />
     </div>
-  )
+  );
 }
